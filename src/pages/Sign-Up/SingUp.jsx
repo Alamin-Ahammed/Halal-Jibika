bbddd937d2969828f55adc1af17047fa2e19f138
Alamin-Ahammed <ahammedalamin2002@gmail.com>
2024-01-03 19:17:08 +0600

    added form validations in sign in and sign up page

--- src/pages/Sign-Up/SingUp.jsx
+++ src/pages/Sign-Up/SingUp.jsx
@@ -32,7 +32,76 @@ const SignUp = () => {
       photoURL: "",
     };
     const image = e.target.profileImage.files[0];
-    const { fullName, email, password } = userInfo;
+    const { fullName, email, password, confirmPassword } = userInfo;
+
+    // Helper function to check valid email format
+    const isValidEmail = (email) => {
+      // Basic email format validation using a regular expression
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
+
+    // Function to validate sign-up form
+    const validateSignUp = (name, email, password, confirmPassword, image) => {
+      const errors = {};
+      const isValidName = (name) => {
+        // Basic check using a regular expression to allow only alphabetical characters and spaces
+        const nameRegex = /^[A-Za-z\s]+$/;
+        return nameRegex.test(name);
+      };
+
+      console.log(isValidName(name))
+
+      if (!name.trim() || !isValidName(name)) {
+        errors.name = "Valid Name is required";
+        toast.error("Valid Name is required", { autoClose: 3000 });
+        return false;
+      }
+
+      if (!email.trim()) {
+        errors.email = "Email is required";
+        toast.error("Email is required", { autoClose: 3000 });
+        return false;
+      } else if (!isValidEmail(email)) {
+        errors.email = "Invalid email format";
+        toast.error("Invalid email format", { autoClose: 3000 });
+        return false;
+      }
+
+      if (!password.trim()) {
+        errors.password = "Password is required";
+        toast.error("Password is required", { autoClose: 3000 });
+        return false;
+      } else if (password.length < 6) {
+        errors.password = "Password must be at least 6 characters";
+        toast.error("Password must be at least 6 characters", {
+          autoClose: 3000,
+        });
+        return false;
+      }
+
+      if (!confirmPassword.trim()) {
+        errors.confirmPassword = "Confirm Password is required";
+        toast.error("Confirm Password is required", { autoClose: 3000 });
+        return false;
+      } else if (password !== confirmPassword) {
+        errors.confirmPassword = "Passwords do not match";
+        toast.error("Passwords do not match", { autoClose: 3000 });
+        return false;
+      }
+
+      if (!image) {
+        errors.image = "Image is required";
+        toast.error("Image is required", { autoClose: 3000 });
+        return false;
+      }
+
+      return true;
+    };
+
+    if (!validateSignUp(fullName, email, password, confirmPassword, image)) {
+      return;
+    }
 
     try {
       await createUserWithEmailAndPassword(auth, email, password);
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -8,30 +8,56 @@ import { toast } from "react-toastify";
 import { useIsLoggedInContext } from "../../Context/IsLoggedInContext";
 
 const SignIn = () => {
-  const {setIsSingedIn} = useIsLoggedInContext();
+  const { setIsSingedIn } = useIsLoggedInContext();
   const navigate = useNavigate();
-  const cookies = new Cookies()
+  const cookies = new Cookies();
   const handleSignIn = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    const {displayName,email,photoURL,uid} = user;
-    cookies.set('authInfo', {displayName,email,photoURL,uid});
-    toast.success('Sign in successful!')
-    setIsSingedIn(true)
-    navigate('/')
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    toast.error(errorMessage)
-  });
+    // Helper function to check valid email format
+    const isValidEmail = (email) => {
+      // Basic email format validation using a regular expression
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
 
-  }
+    const validateSignIn = (email, password) => {
+
+      if (!email.trim()) {
+        toast.warning("Email is required",{autoClose:1000});
+        return false;
+      } else if (!isValidEmail(email)) {
+        toast.error("Invalid email format",{autoClose:1000});
+        return false;
+      }
+
+      if (!password.trim()) {
+        toast.warning("Password is required",{autoClose:1000});
+        return false;
+      }
+
+      return true
+    };
+
+    if (validateSignIn(email, password)) {
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const { displayName, email, photoURL, uid } = user;
+        cookies.set("authInfo", { displayName, email, photoURL, uid });
+        toast.success("Sign in successful!");
+        setIsSingedIn(true);
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+      });
+    }
+  };
   return (
     <div className="signin-container">
       <h2>Sign In</h2>
